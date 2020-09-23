fc3d2b9f22ce0c923e260e8139052cdbf69fdaf8
pmppalm <palm_paper@hotmail.com>
2020-09-23 11:07:58 +0700

    Initial commit

---
+++ src/main/java/com/eservice/eserviceweb/EservicewebApplication.java
@@ -0,0 +1,24 @@
+package com.eservice.eserviceweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+@SpringBootApplication
+public class EservicewebApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "index";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(EservicewebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eservice/eserviceweb/EservicewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eservice.eserviceweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EservicewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
